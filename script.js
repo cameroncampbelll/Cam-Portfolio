c062ec0a2102a0ffc9db4db2983b5e2a2428e346
Cameron Campbell <cameroncampbell@camerons-air.attlocal.net>
2020-10-29 16:00:00 -0400

    FixedtheNavBar

---
+++ script.js
@@ -0,0 +1,25 @@
+let showMenu = (toggleId, navId) => {
+    let toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+
+let navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+
+    let navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
