636e0b3e16e2fc17de4b644172bfc5f20dc8977d
Cameron Campbell <cameronfootball64@gmail.com>
2021-01-03 17:15:20 -0500

    update

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 let showMenu = (toggleId, navId) => {
-    let toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+  let toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle', 'nav-menu')
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
 
-
-let navLink = document.querySelectorAll('.nav_link');
+let navLink = document.querySelectorAll(".nav_link");
 
 function linkAction() {
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
 
-    navLink.forEach(n => n.classList.remove('active'));
-    this.classList.add('active');
-
-
-    let navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
+  let navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(".home_title", {});
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".home_img", { delay: 400 });
+sr.reveal(".home_social-icon", { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal(".about_img", {});
+sr.reveal(".about_subtitle", { delay: 400 });
+sr.reveal(".about_text", { delay: 400 });
+
+/*SCROLL SKILLS*/
+sr.reveal(".skills_subtitle", {});
+sr.reveal(".skills_text", {});
+sr.reveal(".skills_data", { interval: 200 });
+sr.reveal(".skills_img", { delay: 600 });
+
+/*SCROLL WORK*/
+sr.reveal(".work_img", { interval: 200 });
+
+/*SCROLL CONTACT*/
+sr.reveal(".contact_input", { interval: 200 });
